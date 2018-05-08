1810bbac5e84ea07b25b7b54e5f62749943a6e7e
Thiago-Cardoso <trovador182@gmail.com>
2018-05-07 21:34:11 -0300

    trending topics

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class TweetsController < Api::V1::ApiController
+    class TweetsController < Api::V1::ApiController # >
       before_action { current_user }
       before_action :set_tweet, except: %i[create index]
       before_action :authenticate_user, except: [:show, :index]
@@ -16,6 +16,7 @@ module Api
         @tweet = Tweet.new(tweet_params.merge(user: current_user))
 
         if @tweet.save
+          AddHashtagsJob.perform_later(@tweet.body)
           render json: @tweet, status: :created
         else
           render json: { errors: @tweet.errors.full_messages }, status: :unprocessable_entity
@@ -47,10 +48,6 @@ module Api
         def tweet_params
           params.require(:tweet).permit(:body, :tweet_original_id)
         end
-
-      # def set_current_user
-          #@current_user = (current_user)? current_user : nil
-       # end
     end
   end
 end
