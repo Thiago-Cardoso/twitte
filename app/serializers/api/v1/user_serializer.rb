37657023b048e1741d01acb8a3377420ee9cd3d8
Thiago-Cardoso <trovador182@gmail.com>
2018-07-13 22:16:10 -0300

    adjustment in api with elastic search

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,19 +1,19 @@
-module Api
-  module V1
-    class UserSerializer < ActiveModel::Serializer # >
-      attributes :id, :name, :email, :tweets_count, :followers_count, :following_count, :photo
-
+  module Api
+   module V1
+    class UserSerializer < ActiveModel::Serializer
+      attributes :id, :name, :description, :email, :tweets_count, :followers_count, :following_count, :photo
+ 
       def tweets_count
-        object.tweets.count
+       object.tweets.count
       end
-
+ 
       def followers_count
-        object.followers_by_type('User').count
+       object.followers_by_type('User').count
       end
-
+ 
       def following_count
-        object.following_users.count
+       object.following_users.count
       end
+     end
     end
-  end
-end
+   end
